9aff9973b35487923771f42b73523834841f70dd
Yamaishi Akira <yamaishi.a@gmail.com>
2013-04-06 19:39:09 +0900

    ３回め
    ３回め変更

--- testOne.java
+++ testOne.java
@@ -14,8 +14,8 @@ public class testOne {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		System.out.println("Hello GitHub!");
 		System.out.println("Hello GitHub!!");
+		System.out.println("Hello GitHub!!!");
 
 	}
 

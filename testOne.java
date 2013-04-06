2308edc3b8036a9decc0d1618aee5ce226d6f57c
Yamaishi Akira <yamaishi.a@gmail.com>
2013-04-06 19:21:43 +0900

    java first

---
+++ testOne.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+/**
+ * @author yamaishi akira
+ *
+ */
+public class testOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("Hello GitHub!");
+
+	}
+
+}
